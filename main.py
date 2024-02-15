409696a0185ee29033de6e7ef830261e5e22fd6e
muteeburrehman <muteeb285@gmail.com>
2024-02-15 18:32:49 +0500

    login/register apis

---
+++ main.py
@@ -0,0 +1,128 @@
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from functools import wraps
+import jwt
+from datetime import datetime
+
+import models
+import schemas
+from models import Base, User, TokenTable
+
+from utils import create_access_token, create_refresh_token, verify_password, get_hashed_password
+from auth_bearer import JWTBearer
+from database import get_db, engine
+
+app = FastAPI()
+
+
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = "narscbjim@$@&^@&%^&RFghgjvbdsha"  # should be kept secret
+
+
+@app.post("/register")
+async def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter_by(email=user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    encrypted_password = get_hashed_password(user.password)
+    user = User(username=user.username, email=user.email, password=encrypted_password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {"message": "User created successfully"}
+
+
+@app.post('/login', response_model=schemas.TokenSchema)
+def login(request: schemas.requestdetails, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == request.email).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email")
+    hashed_pass = user.password
+    if not verify_password(request.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+
+    access = create_access_token(user.id)
+    refresh = create_refresh_token(user.id)
+
+    token_db = models.TokenTable(user_id=user.id, access_toke=access, refresh_toke=refresh, status=True)
+    db.add(token_db)
+    db.commit()
+    db.refresh(token_db)
+    return {
+        "access_token": access,
+        "refresh_token": refresh,
+    }
+
+
+@app.get('/getusers')
+def getusers(dependencies=Depends(JWTBearer()), session: Session = Depends(get_db)):
+    user = session.query(models.User).all()
+    return user
+
+
+@app.post('/change-password')
+def change_password(request: schemas.changepassword, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == request.email).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
+
+    if not verify_password(request.old_password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid old password")
+
+    encrypted_password = get_hashed_password(request.new_password)
+    user.password = encrypted_password
+    db.commit()
+
+    return {"message": "Password changed successfully"}
+
+
+@app.post('/logout')
+def logout(dependencies=Depends(JWTBearer()), db: Session = Depends(get_db)):
+    token = dependencies
+    payload = jwt.decode(token, JWT_SECRET_KEY, ALGORITHM)
+    user_id = payload['sub']
+    token_record = db.query(models.TokenTable).all()
+    info = []
+    for record in token_record:
+        print("record", record)
+        if (datetime.utcnow() - record.created_date).days > 1:
+            info.append(record.user_id)
+    if info:
+        existing_token = db.query(models.TokenTable).where(TokenTable.user_id.in_(info)).delete()
+        db.commit()
+
+    existing_token = db.query(models.TokenTable).filter(models.TokenTable.user_id == user_id,
+                                                        models.TokenTable.access_toke == token).first()
+    if existing_token:
+        existing_token.status = False
+        db.add(existing_token)
+        db.commit()
+        db.refresh(existing_token)
+    return {"message": "Logout Successfully"}
+
+
+def token_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        payload = jwt.decode(kwargs['dependencies'], JWT_SECRET_KEY, ALGORITHM)
+        user_id = payload['sub']
+        data = kwargs['session'].query(models.TokenTable).filter_by(user_id=user_id, access_toke=kwargs['dependencies'],
+                                                                    status=True).first()
+        if data:
+            return func(kwargs['dependencies'], kwargs['session'])
+
+        else:
+            return {'msg': "Token blocked"}
+
+    return wrapper
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
